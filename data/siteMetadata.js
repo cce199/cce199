bb419f7a79f05cc54f760adea5b019b38b8a0934
cujung <changun.jung@gmail.com>
2022-11-16 21:26:06 +0900

    title change

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: "Jung's personal Blog",
+  author: 'ChangUn Jung',
+  headerTitle: "Jung'sBlog",
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
